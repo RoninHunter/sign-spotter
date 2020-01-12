af57c7d31069db96fbf4c465ee44d79846f1bb71
Jeremy <Jerey80@ufl.edu>
2020-01-11 21:22:18 -0500

    UI beginning

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import NavBar from './components/NavBar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+class App extends Component {
+  render() {
+    return (
+
+
+      <div 
+      
+      // style = {{backgroundColor: '#2a2a2a',
+        //        width: '1920px',
+          //      height: '1080px'}}
+      
+      className="App">
+
+
+        <NavBar />
+
+        {/* <h1>Hello World!</h1> */}
+
+        <p> Sign Spotter is designed as an asset managment application that allows users to identify missing traffic signs. 
+          It is able to accomplish this through the use of a convolutional neural network that identifies the signs from user uploaded videos.</p>
+
+      </div>
+    );
+  }
+}
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div className = "navbar navbar-expand-lg navbar-light bg-light">
+
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+
+  <Navbar.Brand href = "#home"> SignSPOTTER </Navbar.Brand>
+
+  <Navbar.Toggle aria-controls = "responsive-navbar-nav" />
+
+  <Navbar.Collapse id = "responsive-navbar-nav">
+
+    <Nav className = "mr-auto">
+      <Nav.Link href = "#uploadVideo"> Upload Video </Nav.Link>
+      <Nav.Link href = "#status"> Status </Nav.Link>
+      <Nav.Link href = "#about"> About </Nav.Link>
+      <Nav.Link href = "#history"> History </Nav.Link>
+    </Nav>
+
+  </Navbar.Collapse>
+
+</Navbar>
+
+
+      </div>
+    );
+  }
+}
+export default NavBar;
